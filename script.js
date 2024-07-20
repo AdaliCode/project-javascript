414dbdcbfa8b6b79d6d6d854512e2fa97a435b1d
AdaliCode <fiezayyan@gmail.com>
2024-07-20 09:53:30 +0700

    membuat prototype tambah produk

---
+++ script.js
@@ -0,0 +1,17 @@
+var produk = [];
+var beliProduk = function (namaProduk, produk) {
+  // jika slot produk kosong
+  //    tambah produk di awal array
+  //    kembalikan isi array & keluar dari function
+  //  else
+  //    telurusuri seluruh slot produk dari awal
+  //        jika ada produk kosong
+  //            tambah produk di slot tersebut
+  //            kembalikan isi array & keluar dari function
+  //        jika sudah ada nama produk yang sama
+  //            tampilkan pesan kesalahannya
+  //            kembalikan isi array & keluar dari function
+  //        jika seluruh slot produk terisi
+  //            tambah produk di akhir array
+  //            kembalikan isi array & keluar dari function
+};
