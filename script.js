97cb25f451bf1c42c7d7b76c68abec20bff21e00
AdaliCode <fiezayyan@gmail.com>
2024-07-21 07:42:47 +0700

    membuat object menu

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-var menu = ["Kue Lumpur", undefined, "Kue Sus"];
-var tambahProduk = function (namaProduk) {
-  if (menu.length == 0) {
-    menu.push(namaProduk);
-    return menu;
-  } else {
-    for (let i = 0; i < menu.length; i++) {
-      if (menu[i] == undefined) {
-        menu[i] = namaProduk;
-        return menu;
-      } else if (menu[i] == namaProduk) {
-        console.log(namaProduk + " sudah ada di dalam slot");
-        return menu;
-      } else if (i == menu.length - 1) {
-        menu.push(namaProduk);
-        return menu;
-      }
-    }
+function Menu(namaProduk, jumlahProduk, hargaJual, hasilJual) {
+  this.namaProduk = namaProduk;
+  this.jumlahProduk = jumlahProduk;
+  this.hargaJual = hargaJual;
+  this.hasilJual = hasilJual;
+}
+// kalau pelanggan beli
+Menu.prototype.pelangganBeli = function (jumlahBeli) {
+  if (jumlahBeli > this.jumlahProduk) {
+    return "Jumlah Produk Kurang!";
   }
+  this.hasilJual += this.hargaJual * jumlahBeli;
+  this.jumlahProduk -= jumlahBeli;
+  return "Selesai memproses produk";
 };
-var hapusProduk = function (namaProduk) {
-  if (menu.length == 0) {
-    console.log("Menu masih Kosong.");
-    return menu;
-  } else {
-    for (let i = 0; i < menu.length; i++) {
-      if (menu[i] == namaProduk) {
-        menu[i] = undefined;
-        return menu;
-      } else if (i == menu.length - 1) {
-        console.log(namaProduk + " tidak ada di Menu.");
-        return menu;
-      }
-    }
+// produknya expired
+Menu.prototype.expired = function (jumlahProduk) {
+  if (jumlahProduk > this.jumlahProduk) {
+    return "Produknya tidak sebanyak itu!";
   }
+  this.jumlahProduk -= jumlahProduk;
+  return "Selesai memproses produk";
 };
+// tambah menu
+// jalankan/run di console sesuai nama let nya
+// contoh: kueLumpur;
+// contoh: kueLumpur.hargaJual;
+let kueLumpur = new Menu("Kue Lumpur", 10, 2500, 0);
